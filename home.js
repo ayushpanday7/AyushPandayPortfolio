b931117ff8ae80f3dfd80be6c33704a08f50e608
ayush <theflashvs959@gmail.com>
2023-07-12 09:35:03 +0530

    setting Home page

--- home.js
+++ home.js
@@ -1,6 +1,41 @@
 window.addEventListener('load', function() {
-    var preloader = document.getElementById('preloader');
-    preloader.style.display = 'none';
-    document.body.style.overflow = 'auto';
-  });
-  
+  var preloader = document.getElementById('preloader');
+  preloader.style.display = 'none';
+  document.body.style.overflow = 'visible';
+});
+function showSideBar(){
+  document.querySelector(".hide").style = "display:none";
+    setTimeout(() => {
+      document.querySelector(".show").style = "display:flex;";
+    }, 900); 
+}
+window.matchMedia('(max-width: 1000px)').addEventListener('change', (event) => {
+  if (event.matches) {
+    document.querySelector(".sidebar").classList.add("hidesidebar");
+    document.querySelector(".hide").style.display = "none";
+    document.querySelector(".show").style.display = "flex";
+  } else {
+    document.querySelector(".sidebar").classList.remove("hidesidebar");
+    setTimeout(() => {
+      document.querySelector(".hide").style = "display:flex;";
+    }, 1000); 
+    document.querySelector(".show").style.display = "none";
+  }
+});
+document.querySelector(".hamburger").addEventListener("click",()=>{
+  var sidebar = document.querySelector(".sidebar").classList;
+  sidebar.toggle("hidesidebar");
+  if(sidebar.contains("hidesidebar"))
+  {
+    document.querySelector(".hide").style = "display:none";
+    setTimeout(() => {
+      document.querySelector(".show").style = "display:flex;";
+    }, 900);  
+  }
+  else{
+    document.querySelector(".show").style = "display:none";
+    setTimeout(() => {
+      document.querySelector(".hide").style = "display:flex;";
+    }, 1000);  
+  }
+})
