347e218dbf7edb9650b8a8383b5a71944433b137
ayush <theflashvs959@gmail.com>
2023-07-13 07:54:52 +0530

    try to fix more

--- home.js
+++ home.js
@@ -1,4 +1,19 @@
-
+window.addEventListener('load', function() {
+  var preloader = document.getElementById('preloader');
+  preloader.style.display = 'none';
+  document.body.style.overflow = 'visible';
+});
+window.matchMedia('(max-width: 1000px)').addEventListener('change', (event) => {
+  if (event.matches) {
+    document.querySelector(".sidebar").classList.add("hidesidebar");
+    document.querySelector(".hide").style.display = "none";
+    document.querySelector(".show").style.display = "flex";
+  } else {
+    document.querySelector(".sidebar").classList.remove("hidesidebar");
+    document.querySelector(".hide").style.display = "flex";
+    document.querySelector(".show").style.display = "none";
+  }
+});
 document.querySelector(".hamburger").addEventListener("click",()=>{
   var sidebar = document.querySelector(".sidebar").classList;
   sidebar.toggle("hidesidebar");
